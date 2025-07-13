752cbeab18de6fd6bec47eed4cce34e2e1790d67
kanduladhanush <sivadhanush1112@gmail.com>
2025-07-14 02:42:01 +0530

    Remove node_modules from repository

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,3 @@
-// src/App.jsx
-import React from "react";
-import QuoteGenerator from "./components/QuoteGenerator";
-
-export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
-      <QuoteGenerator />
-    </div>
-  );
+export default function hello() {
+  return 'Hello, World!';
 }
